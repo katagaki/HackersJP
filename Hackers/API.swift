4eda8830084c9452f34a2c42c0d0c12c16533e69
katagaki <68231035+katagaki@users.noreply.github.com>
2023-08-22 12:20:26 +0800

    Initial commit

---
+++ Hackers/API.swift
@@ -0,0 +1,10 @@
+//
+//  API.swift
+//  Hackers
+//
+//  Created by シンジャスティン on 2023/08/22.
+//
+
+import Foundation
+
+let apiEndpoint = "https://hacker-news.firebaseio.com/v0"
--- Hackers/ContentView.swift
+++ Hackers/ContentView.swift
@@ -5,17 +5,40 @@
 //  Created by シンジャスティン on 2023/08/22.
 //
 
+import Alamofire
 import SwiftUI
 
 struct ContentView: View {
+
+    @State var storyIDs: [Int] = []
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            NavigationStack {
+                List(storyIDs, id: \.description, rowContent: { storyID in
+                    Text(storyID.description)
+                })
+                .task {
+                    do {
+                        storyIDs = try await AF.request("https://hacker-news.firebaseio.com/v0/topstories.json",
+                                                        method: .get)
+                        .serializingDecodable([Int].self,
+                                              decoder: JSONDecoder()).value
+                    } catch {
+                        debugPrint(error.localizedDescription)
+                    }
+                }
+            }
+            .tabItem {
+                Text("記事")
+                Image(systemName: "newspaper.fill")
+            }
+            LicensesView()
+                .tabItem {
+                    Text("著者権")
+                    Image(systemName: "books.vertical.fill")
+                }
         }
-        .padding()
     }
 }
 
