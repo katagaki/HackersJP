21c587e5fa0499fe6cc4927360f770f1c5472493
katagaki <68231035+katagaki@users.noreply.github.com>
2023-08-22 11:32:37 +0800

    Initial Commit

---
+++ Hackers/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hackers
+//
+//  Created by シンジャスティン on 2023/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hackers/HackersApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackersApp.swift
+//  Hackers
+//
+//  Created by シンジャスティン on 2023/08/22.
+//
+
+import SwiftUI
+
+@main
+struct HackersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
