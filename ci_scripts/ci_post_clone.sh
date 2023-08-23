52258834adf47201c6ad7c098fd194a454e8b8fe
katagaki <68231035+katagaki@users.noreply.github.com>
2023-08-23 15:02:51 +0800

    Create files for Xcode Cloud integration

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#  ci_post_clone.sh
+#  Hackers
+#
+#  Created by シンジャスティン on 2023/08/23.
+#
+
+set -e
+
+echo "Installing CocoaPods."
+brew install cocoapods
+
+echo "Installing pods."
+cd $CI_WORKSPACE/
+pod install
+
+echo "Post-clone script completed."
+exit 0
